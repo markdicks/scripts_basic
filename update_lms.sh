8a3e9f4b8be054f930693ef393b2ba3fca6e9fbf
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-08 22:59:50 +0200

    Removing messages been displayed

--- update_lms.sh
+++ update_lms.sh
@@ -4,18 +4,15 @@ script_name="wtc-lms"
 
 # Check if the script exists in any directory
 if whereis -b "$script_name"; then
-  echo "Script exists in the following directories:"
-  whereis -b "$script_name"
-  echo "Removing old versions of the script..."
   # Remove the script from all directories where it is found
-  sudo rm -rf $(whereis -b "$script_name" | cut -d" " -f2-)
+  sudo rm -rf $(whereis -b "$script_name" | cut -d" " -f2-) >/dev/null 2>&1
 fi
 
 # Move the script to /bin
-sudo mv $(pwd)/wtc_scripts/"$script_name" /bin/
+sudo mv $(pwd)/wtc_scripts/"$script_name" /bin/ >/dev/null 2>&1
 
 # Make the script executable
-sudo chmod +x /bin/"$script_name"
-rm -rf wtc_scripts/
+sudo chmod +x /bin/"$script_name" >/dev/null 2>&1
+rm -rf wtc_scripts/ >/dev/null 2>&1
 echo "Script moved and made executable."
 wtc-lms --version
