e435e3362e346e8f5304aa94c09e4d22619ad503
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-09 11:56:52 +0200

    Adding error messages

--- update_lms.sh
+++ update_lms.sh
@@ -14,5 +14,16 @@ sudo mv $HOME/Downloads/"$script_name" /bin/ >/dev/null 2>&1
 # Make the script executable
 sudo chmod +x /bin/"$script_name" >/dev/null 2>&1
 rm -rf wtc_scripts/ >/dev/null 2>&1
-echo "LMS was updated succesfully"
-wtc-lms --version
+# Run the command and capture its output
+output=$(wtc-lms --version)
+
+# Check if the output contains a certain string
+if [[ $output == *"wtc_scripts/update_lms.sh: 18: wtc-lms: not found"* ]]; then
+  echo "Error: Please make sure you download wtc-lms from slack"
+  echo "https://app.slack.com/client/T016EBFH7F0/C01QPC5DLBA"
+  echo "Make sure it goes under the Downloads directory"
+else
+  echo "LMS was updated succesfully"
+  echo "$output"
+fi
+
