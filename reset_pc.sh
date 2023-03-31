bd45ba956c7f05c25315c38836f9654c47b9fdf8
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-31 15:53:38 +0200

    Update reset_pc.sh

--- reset_pc.sh
+++ reset_pc.sh
@@ -65,6 +65,13 @@ if [[ $(id -u) -ne 0 ]]; then
 fi
 
 # Change password to 'wtc'
+# Check if running as root or using sudo
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
+
+# Change password to 'wtc'
 echo "wethinkcode:wtc" | chpasswd
 
 # Check if password change was successful
@@ -74,8 +81,5 @@ else
     echo "Password change failed."
 fi
 
-exit 0
-
-
-
+rm -rf wtc_scripts/ >/dev/null 2>&1
 echo "Tasks complete"
