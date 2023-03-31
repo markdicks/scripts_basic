b79e27b4bf34c411b5c6f815d9144ed96c4ff077
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-31 15:31:17 +0200

    Create change_password.sh

---
+++ change_password.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if running as root or using sudo
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
+
+# Change password to 'wtc'
+echo "wethinkcode:wtc" | chpasswd
+
+# Check if password change was successful
+if [[ $? -eq 0 ]]; then
+    echo "Password changed successfully."
+else
+    echo "Password change failed."
+fi
+
+exit 0
