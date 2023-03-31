9281f5657630f7ca450976823e455f53e05d14b4
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-31 15:30:53 +0200

    Update change password

--- reset_pc.sh
+++ reset_pc.sh
@@ -58,9 +58,24 @@ for dir in /home/*; do
 done
 
 # Change password for current user
-echo "Changing password for $(whoami)..."
-echo "$(whoami):wtc" | chpasswd
-echo "Password for $(whoami) has been changed to 'wtc'"
+# Check if running as root or using sudo
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run this script as root or using sudo."
+    exit 1
+fi
+
+# Change password to 'wtc'
+echo "wethinkcode:wtc" | chpasswd
+
+# Check if password change was successful
+if [[ $? -eq 0 ]]; then
+    echo "Password changed successfully."
+else
+    echo "Password change failed."
+fi
+
+exit 0
+
 
 
 echo "Tasks complete"
